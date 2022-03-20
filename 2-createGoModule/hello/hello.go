728458a00170a9bc504883d00018c8ff61e66784
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2022-03-20 15:11:03 -0300

    feat: return a random greeting

--- 2-createGoModule/hello/hello.go
+++ 2-createGoModule/hello/hello.go
@@ -11,8 +11,8 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	// message, err := greetings.Hello("Foo")
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Foo")
+	// message, err := greetings.Hello("")
 
 	if err != nil {
 		log.Fatal(err)
